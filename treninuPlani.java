c33a409913ad92ddd8ee9b155367697401b1ba23
24DP2SCaun <a240352sc@rvt.lv>
2026-04-07 10:08:15 +0000

    pievienota filtrēšanas metode kodam

--- treninuPlani.java
+++ treninuPlani.java
@@ -2,72 +2,112 @@ import java.io.*;
 import java.util.*;
 
 public class treninuPlani {
-    private static ArrayList<String> treninuList = new ArrayList<>();
-    private static final String filepathforTrenini = "csv/treninuPlaniglab.csv";
 
-    public static void treninuPlanaIevade() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Ievadiet muskuļu grupu:");
-        String muskuluGrupa = scanner.nextLine();
-        System.out.println("Ievadiet treniņa datumu: ");
-        String treninaDatums = scanner.nextLine();
-        System.out.println("Ievadiet treniņa ilgumu:");
-        String treninaIlgums = scanner.nextLine();
-        System.out.println("Ievadiet treniņa aprakstu: ");
-        String treninaApraksts = scanner.nextLine();
-        System.out.println("Ievadiet brīvo vietu skaitu:");
-        String brivoVietuSkaits = scanner.nextLine();
-        int newID = getIDtreninam() + 1;
+        private static ArrayList<String> treninuList = new ArrayList<>();
+        private static final String filepathforTrenini = "csv/treninuPlaniglab.csv";
 
-        String treninaData = newID + "," + muskuluGrupa + "," + treninaDatums + "," + treninaIlgums + "," + treninaApraksts + "," + brivoVietuSkaits;
-        treninuList.add(treninaData);
+        public static void treninuPlanaIevade() {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Ievadiet treniņa nosaukumu:");
+                String treninaNosaukums = scanner.nextLine();
+                System.out.println("Ievadiet sporta veidu(klinšu kāpšana, kalistēnika):");
+                String sportaVeids = scanner.nextLine();
+                System.out.println("Ievadiet grūtības pakāpi(viegls, vidējs, grūts):");
+                String grutibasPakape = scanner.nextLine();
+                System.out.println("Ievadiet muskuļu grupu:");
+                String muskuluGrupa = scanner.nextLine();
+                System.out.println("Ievadiet treniņa datumu: ");
+                String treninaDatums = scanner.nextLine();
+                System.out.println("Ievadiet treniņa ilgumu:");
+                String treninaIlgums = scanner.nextLine();
+                System.out.println("Ievadiet treniņa aprakstu: ");
+                String treninaApraksts = scanner.nextLine();
+                System.out.println("Ievadiet brīvo vietu skaitu:");
+                String brivoVietuSkaits = scanner.nextLine();
+                int newID = getIDtreninam() + 1;
 
-        updateFileTrenini();
+                String treninaData = newID + "," + treninaNosaukums + "," + sportaVeids + "," + grutibasPakape + "," + muskuluGrupa + "," + treninaDatums + "," + treninaIlgums + ","
+                                + treninaApraksts + "," + brivoVietuSkaits;
+                treninuList.add(treninaData);
 
-        scanner.close();
-    }
+                updateFileTrenini();
 
-    public static int getIDtreninam() {
-        if (treninuList.isEmpty()){
-            return 0;
+                scanner.close();
         }
 
-        String lastLine = treninuList.get(treninuList.size() - 1);
-        String[] parts = lastLine.split(",");
-        String idPart = parts[0].trim();
-        if (idPart.endsWith(".")) {
-            idPart = idPart.substring(0, idPart.length() - 1).trim();
-        }
-        try {
-            return Integer.parseInt(idPart);
-        } catch (NumberFormatException e) {
-            return 0;
-        }
-    }
+        public static int getIDtreninam() {
+                if (treninuList.isEmpty()) {
+                        return 0;
+                }
 
-    public static void updateFileTrenini() {
-        Scanner scanner = new Scanner(System.in);
-        try {
-            BufferedWriter writer = new BufferedWriter(
-                new FileWriter(filepathforTrenini));
-
-            writer.write("id, muskuluGrupa, treninaDatums, treninaIlgums, treninaApraksts, brivoVietuSkaits");
-            writer.newLine();
-
-            for (String trenina : treninuList) {
-                writer.write(trenina);
-                writer.newLine();
-            }
-
-            writer.close();
-        } catch (IOException e) {
-            System.out.println(
-                "An error occured while writing this file: "
-            + e.getMessage());
+                String lastLine = treninuList.get(treninuList.size() - 1);
+                String[] parts = lastLine.split(",");
+                String idPart = parts[0].trim();
+                if (idPart.endsWith(".")) {
+                        idPart = idPart.substring(0, idPart.length() - 1).trim();
+                }
+                try {
+                        return Integer.parseInt(idPart);
+                } catch (NumberFormatException e) {
+                        return 0;
+                }
         }
 
-        scanner.close();
-        
-    }
-}
+        public static void updateFileTrenini() {
+                Scanner scanner = new Scanner(System.in);
+                try {
+                        BufferedWriter writer = new BufferedWriter(
+                                        new FileWriter(filepathforTrenini));
 
+                        writer.write("id, treninaNosaukums, sportaVeids, grutibasPakape, muskuluGrupa, treninaDatums, treninaIlgums, treninaApraksts, brivoVietuSkaits");
+                        writer.newLine();
+
+                        for (String trenina : treninuList) {
+                                writer.write(trenina);
+                                writer.newLine();
+                        }
+
+                        writer.close();
+                } catch (IOException e) {
+                        System.out.println(
+                                        "An error occured while writing this file: "
+                                                        + e.getMessage());
+                }
+
+                scanner.close();
+
+        }
+
+        public static void searchByTypeandDifficulty() {
+
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("Ievadiet sporta veidu: ");
+                String Sport = scanner.nextLine(). trim(). toLowerCase();
+
+                System.out.println("Ievadiet grūtības pakāpi: ");
+                String Difficulty = scanner.nextLine(). trim(). toLowerCase();
+
+                boolean found = false;
+
+                        System.out.println("-----ATRASTIE TRENIŅI-----");
+
+                for (String line: treninuList) {
+                        
+                        String[] parts = line.split(",");
+                        if (parts.length < 8) continue;
+
+                        String sportaVeids = parts[1].trim().toLowerCase();
+                        String grutibasPakape = parts[2].trim().toLowerCase();
+
+                        if (sportaVeids.equals(Sport) && grutibasPakape.equals(Difficulty)) {
+                                        System.out.println(line);
+                        found = true;
+                        }
+                                }
+                if (!found) {
+                System.out.println("Treniņš un/vai grūtības pakāpe netika atrasta.");
+                        }
+                scanner.close();
+                }
+        }
