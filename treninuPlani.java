0fa85a60cb989011ec53482e362a2671c02dc97c
24DP2SCaun <a240352sc@rvt.lv>
2026-04-08 07:11:48 +0000

    izlabot iepriekš uzrakstītās funkcijas, izņemot paradaPieteikusosTreninus, kurš nav pabeigts

--- treninuPlani.java
+++ treninuPlani.java
@@ -6,8 +6,14 @@ public class treninuPlani {
         private static ArrayList<String> treninuList = new ArrayList<>();
         private static final String filepathforTrenini = "csv/treninuPlaniglab.csv";
 
+        private static ArrayList<String> pieteikumi = new ArrayList<>();
+        private static final String filepathforPieteikumi = "csv/pieteikumi.csv";
+
+        /*------------------TRENIŅA IZVEIDE-------------------------- */
+
         public static void treninuPlanaIevade() {
                 Scanner scanner = new Scanner(System.in);
+
                 System.out.println("Ievadiet treniņa nosaukumu:");
                 String treninaNosaukums = scanner.nextLine();
                 System.out.println("Ievadiet sporta veidu(klinšu kāpšana, kalistēnika):");
@@ -35,6 +41,8 @@ public class treninuPlani {
                 scanner.close();
         }
 
+        /*------------------TRENIŅA ID PIEŠĶIRŠANA-------------------------- */
+
         public static int getIDtreninam() {
                 if (treninuList.isEmpty()) {
                         return 0;
@@ -53,8 +61,10 @@ public class treninuPlani {
                 }
         }
 
+        /*------------------TRENIŅA FAILA (CSV) SAGLABĀŠANA-------------------------- */
+        
         public static void updateFileTrenini() {
-                Scanner scanner = new Scanner(System.in);
+                
                 try {
                         BufferedWriter writer = new BufferedWriter(
                                         new FileWriter(filepathforTrenini));
@@ -70,13 +80,13 @@ public class treninuPlani {
                         writer.close();
                 } catch (IOException e) {
                         System.out.println(
-                                        "An error occured while writing this file: "
-                                                        + e.getMessage());
+                                        "An error occured while writing this file: " + e.getMessage());
                 }
 
-                scanner.close();
 
         }
+
+        /*---------------------TRENIŅU FILTRĒŠANA---------------------- */
 
         public static void searchByTypeandDifficulty() {
 
@@ -95,10 +105,10 @@ public class treninuPlani {
                 for (String line: treninuList) {
                         
                         String[] parts = line.split(",");
-                        if (parts.length < 8) continue;
+                        if (parts.length < 9) continue;
 
-                        String sportaVeids = parts[1].trim().toLowerCase();
-                        String grutibasPakape = parts[2].trim().toLowerCase();
+                        String sportaVeids = parts[2].trim().toLowerCase();
+                        String grutibasPakape = parts[3].trim().toLowerCase();
 
                         if (sportaVeids.equals(Sport) && grutibasPakape.equals(Difficulty)) {
                                         System.out.println(line);
@@ -110,6 +120,8 @@ public class treninuPlani {
                         }
                 scanner.close();
                 }
+
+                /*---------------------TRENIŅU PLĀNU IZVADE---------------------- */
 
                 public static void paradaTreninuPlanus() {  //*parāda visus treniņus */
 
@@ -124,16 +136,21 @@ public class treninuPlani {
                                 String[] parts = line.split(",");
 
                                 System.out.println("ID: " + parts[0]);
-                                System.out.println("Sporta veids: " + parts[1]);
-                                System.out.println("Grūtības pakāpe: " + parts[2]);
-                                System.out.println("Treniņa datums: " + parts[3]);
-                                System.out.println("Treniņa ilgums: " + parts[4]);
-                                System.out.println("Treniņa apraksts: " + parts[5]);
-                                System.out.println("Brīvās vietas: " + parts[6]);
+                                System.out.println("Nosaukums: " + parts[1]);
+                                System.out.println("Sporta veids: " + parts[2]);
+                                System.out.println("Grūtības pakāpe: " + parts[3]);
+                                System.out.println("Muskuļu grupa: " + parts[4]);
+                                System.out.println("Datums: " + parts[5]);
+                                System.out.println("Ilgums: " + parts[6]);
+                                System.out.println("Apraksts: " + parts[7]);
+                                System.out.println("Brīvās vietas: " + parts[8]);
+
                         
                                 System.out.println("-----------------------------");
                         }
                 }
+
+                /*---------------------PARĀDA TRENIŅUS, KUROS IR PIETEICIES---------------------- */
 
                 public static void paradaPieteikusosTreninus () {
 
@@ -147,25 +164,32 @@ public class treninuPlani {
                         }
 
                         if (!found) {
-                                System.out.println("Tu neesi pieteicies nevienam treniņam!");
+                                System.out.println("Tu neesi pieteicies nevienam treniņam!"); }
                         }
+
+
+
+                        /*---------------PĀRBAUD VAI TRENIŅŠ IR PILNS---------------------- */
 
                         public static boolean parbaudaVaiTreninsPilns(String treninaData) {
                                 String[] parts = treninaData.split(",");
-                                int brivasVietas = Integer.parseInt(parts[7].trim());
+                                int brivasVietas = Integer.parseInt(parts[8].trim());
                                 return brivasVietas <= 0;
                         }
 
+                        /*---------------ATJAUNO BRĪVĀS VIETAS---------------------- */
+
                         public static void updateBrivasVietas(int treninaID) {
+                                
                                 for (int i = 0; i < treninuList.size(); i++) {
-                                       
+                                        String[] parts = treninuList.get(i).split(",");
                                         int id = Integer.parseInt(parts[0].trim());
 
-                                        if (id = treninaID) {
+                                        if (id == treninaID) {
 
-                                                int brivasVietas = Integer.parseInt(parts[7].trim());
+                                                int brivasVietas = Integer.parseInt(parts[8].trim());
                                                 brivasVietas--;
-                                                parts[7] = String.valueOf(brivasVietas);
+                                                parts[8] = String.valueOf(brivasVietas);
 
                                                 String updatedTreninaData = String.join(",", parts);
                                                 treninuList.set(i, updatedTreninaData);
@@ -173,6 +197,10 @@ public class treninuPlani {
                                                 return;
                                         }
                         }
+
+                }
+
+                 /*---------------------PIETEIKŠANĀS TRENIŅAM---------------------- */
 
                 public static void pieteiktiesTreninam (String userID, int treninaID, boolean isTreneris) {
 
@@ -206,10 +234,33 @@ public class treninuPlani {
 
                         String pieteikums = userID + "," + treninaID;
                         pieteikumi.add(pieteikums);
-                        updatePieteikumiFile();
+                        updatePieteikumiFailu();
 
                         updateBrivasVietas(treninaID);
                         System.out.println("Tu esi veiksmīgi pieteicies treniņam ar ID " + treninaID + ".");
                 }
 
+                /*---------------------PIETEIKUMU FAILA (CSV) SAGLABĀŠANA---------------------- */
+
+                public static void updatePieteikumiFailu() {
+
+                        try {
+
+                                BufferedWriter writer = new BufferedWriter(new FileWriter(filepathforPieteikumi));
+
+                                writer.write("userID, treninaID");
+                                writer.newLine();
+
+                                for (String pieteikums : pieteikumi) {
+                                        writer.write(pieteikums);
+                                        writer.newLine();
+                                }
+
+                                writer.close();
+                        } catch (IOException e) {
+                                System.out.println("Radās kļūda rakstot pieteikum failā: " + e.getMessage());
+                }
+
         }
+
+                }
